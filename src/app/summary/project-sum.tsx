83a29d37a31175df17015a83316a7e41c80c5b43
Jake Pole <jakepole@hotmail.co.uk>
2025-11-07 20:26:58 +0000

    Adding Omarchy Dotfiles repo

--- src/app/summary/project-sum.tsx
+++ src/app/summary/project-sum.tsx
@@ -16,9 +16,11 @@ const ProjSummary = () => {
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
             </svg>
-            <a href="https://github.com/JakeP2400/COMP3010-Security-Operations-Incident-Management" className="text-grey-700 hover:underline">Repository</a>
+            <a href="https://github.com/JakeP2400/BadUSB" className="text-grey-700 hover:underline">Repository</a>
           </p>
         </div>
+
+
         {/* Project 2 */}
         <div className="flex-1 flex flex-col justify-center items-center bg-blue2 p-4 dark:bg-gray-600 dark:text-gray-300 rounded-xl">
           <h1 className="hover:underline text-lg font-bold mb-2 dark:text-white">PCAP Analysis</h1>
@@ -27,13 +29,21 @@ const ProjSummary = () => {
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
             </svg>
-            <a href="https://github.com/JakeP2400/BadUSB" className="text-grey-700 hover:underline">Repository</a>
+            <a href="https://github.com/JakeP2400/COMP3010-Security-Operations-Incident-Management" className="text-grey-700 hover:underline">Repository</a>
           </p>
         </div>
+
+
         {/* Project 3 */}
         <div className="flex-1 flex flex-col justify-center items-center bg-blue2 p-4 dark:bg-gray-600 dark:text-gray-300 rounded-xl">
-          <h1 className="hover:underline text-lg font-bold mb-2 dark:text-white">Keylogger</h1>  
-          <p className="text-center">Keyloggers are very useful tools, and I am currently exploring by building my own</p>
+          <h1 className="hover:underline text-lg font-bold mb-2 dark:text-white">My Omarchy Setup</h1>  
+          <p className="text-center">Omarchy is an opinionated version of Arch Linux, within this Repository I have been building my own dotfiles (for configureation of the styling)</p>
+          <p className="mt-2 text-gray-800 dark:text-gray-300 flex items-center space-x-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+            </svg>
+            <a href="https://github.com/JakeP2400/Omarchy-dotfiles" className="text-grey-700 hover:underline">Repository</a>
+          </p>
         </div>
       </div>
     </div>
